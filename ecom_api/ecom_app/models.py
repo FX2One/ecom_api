c6c10a00096172b2bf8dba638cf619af7e35205e
FX2One <dfoxik@gmail.com>
2023-05-21 23:23:08 +0200

    ADD: basic model overlay
    ADD: path to templates
    
    ADD: imports and urls for serving Static and Media in development state
    
    ADD: MPTT package, TFK relationship between models and str returns to models
    
    ADD: basic serializers to models

--- ecom_api/ecom_app/models.py
+++ ecom_api/ecom_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    class MPTTMeta:
+        level_attr = 'mptt_level'
+        order_insertion_by=['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    
+
+    def __str__(self):
+        return self.name
+
+class ProductLine(models.Model):
+    pass
+
+class ProductImage(models.Model):
+    pass
+
+class Attribute(models.Model):
+    pass
+
+class AttributeValue(models.Model):
+    pass
+
