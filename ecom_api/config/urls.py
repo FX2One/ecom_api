957216490e4ba0969eb792b5d3aaafc207f3e324
FX2One <dfoxik@gmail.com>
2023-05-22 15:56:13 +0200

    UPDATE: Viewsets for basic views using serializer_class
    ADD: registering router to catch views as routers

--- ecom_api/config/urls.py
+++ ecom_api/config/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from ecom_app.views import CategoryViewSet
+
+router = DefaultRouter()
+router.register(r'category', CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/", include(router.urls)),
 ]
 
 if settings.DEBUG:
--- ecom_api/ecom_app/views.py
+++ ecom_api/ecom_app/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
-
+from .models import Category, Product, Brand
+from .serializers import CategorySerializer, ProductSerializer, BrandSerializer
+from rest_framework.response import Response
+from rest_framework import viewsets
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class BrandViewSet(viewsets.ViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
